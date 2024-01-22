3ae11d43176b6dff98d133e67a2db1f37b7f6faf
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-01-22 01:11:30 +0100

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app that helps you find latest board and video gaming events in Belgrade, Serbia</p>
+        <p>Version 1.0.0</p>
+    </div>
+  )
+}
+
+export default AboutPage
