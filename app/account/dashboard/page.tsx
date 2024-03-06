f2c36288cc5ade72cc73bbf0cfae99d1c3bfdf53
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-03-06 19:34:41 +0100

    Fetch user events for dashboard

--- app/account/dashboard/page.tsx
+++ app/account/dashboard/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { API_URL } from "@/config";
+import { cookies } from "next/headers";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const token = cookies().get("token") ? cookies().get("token")?.value : "" 
+  console.log(token);
+  const res = await fetch(`${API_URL}/api/my-events`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+  console.log(events);
+
   return (
     <div>
-        <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
